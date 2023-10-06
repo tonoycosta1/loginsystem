d44733f2beb75185b4958dce9c6d77ed174ae74a
tonoycosta1 <48320644+tonoycosta1@users.noreply.github.com>
2023-10-06 16:10:58 +0000

    DEMO

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid px-4">
-                        <h1 class="mt-4">   User Registration & Login and User Management System With admin panel</h1>
+                        <h1 class="mt-4">   User Registration & Login and User Management System With</h1>
                         <ol class="breadcrumb mb-4">
                             <li class="breadcrumb-item"><a href="index.php">Home</a></li>
               
